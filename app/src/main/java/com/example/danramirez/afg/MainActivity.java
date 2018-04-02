f82c8af13c8777d8394460205a6ca616bab992ae
danramirez2001 <dramirez@wpi.edu>
2018-04-02 12:09:33 -0400

    Firebase Integration

--- app/src/main/java/com/example/danramirez/afg/MainActivity.java
+++ app/src/main/java/com/example/danramirez/afg/MainActivity.java
@@ -2,6 +2,16 @@ package com.example.danramirez.afg;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,6 +20,8 @@ import android.widget.Spinner;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity  implements AdapterView.OnItemSelectedListener{
+
+    private ArrayList<Job> jobs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +40,39 @@ public class MainActivity extends AppCompatActivity  implements AdapterView.OnIt
 
         //Apply Adapter to Spinner
         spinner.setAdapter(adapter);
+
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+
+        jobs = new ArrayList<Job>();
+        readSongData();
+
+        for(Job job: jobs) {
+            Log.d("MainActivity", job.getTitle() + " " + job.getCompany() + " " + job.getDescription() + " " + job.getAddress());
+            database.child("JobListing").setValue(job);
+        }
+
+
+
+    }
+
+
+
+    private void readSongData() {
+        // Read data from file
+        InputStream is = getResources().openRawResource(R.raw.joblist);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+        String line = "";
+        try {
+            while((line = reader.readLine()) != null) {
+                // Split by ','
+                String[] fields = line.split("'''");
+                Job s = new Job(fields[0], fields[1], fields[2], fields[3]);
+                jobs.add(s);
+            }
+        } catch(IOException e) {
+            Log.e("MainActivity", "Error reading data on line " + line);
+        }
 
     }
 
