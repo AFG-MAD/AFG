4e320e286d4ac21c255e33e7bd5b8c8fb303601b
Anna Hazelwood <alhazelwood@wpi.edu>
2018-04-02 11:56:55 -0400

    took the user selected category and put into string selectedCategory

--- app/src/main/java/com/example/danramirez/afg/MainActivity.java
+++ app/src/main/java/com/example/danramirez/afg/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.danramirez.afg;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Constructing the Spinner/Dropdown
        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+       spinner.setOnItemSelectedListener(this);
 
        //Array Adapter
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categories_array, android.R.layout.simple_spinner_item);
@@ -25,6 +28,18 @@ public class MainActivity extends AppCompatActivity {
 
         //Apply Adapter to Spinner
         spinner.setAdapter(adapter);
+
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
+
+        Object category = parent.getItemAtPosition(pos);
+        String selectedCategory = category.toString();
+
+    }
+
+    public void onNothingSelected(AdapterView<?> parent){
+
 
     }
 
