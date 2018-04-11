b4d49b3c7f1dddcd6c53a6e8523f2a8c109c66e9
danramirez2001 <dramirez@wpi.edu>
2018-04-11 12:42:21 -0400

    MainActivity

--- app/src/main/java/com/example/danramirez/afg/MainActivity.java
+++ app/src/main/java/com/example/danramirez/afg/MainActivity.java
@@ -9,6 +9,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.io.BufferedReader;
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity  implements AdapterView.OnIt
 
     private ArrayList<Job> jobs;
     final DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+    boolean jobExists;
 
 
     @Override
@@ -48,21 +50,17 @@ public class MainActivity extends AppCompatActivity  implements AdapterView.OnIt
         spinner.setAdapter(adapter);
 
 
-
-        jobs = new ArrayList<Job>();
         readJobData();
-        for(Job job: jobs)
-        {
-            DatabaseReference pushedJobListing = database.child("JobListings").push();
-            job.setID(pushedJobListing.getKey());
-            pushedJobListing.setValue(job);
-        }
+
+
+
+
 
         database.child("JobListings").addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
                 Job job = dataSnapshot.getValue(Job.class);
-                System.out.println(job.toString());
+                //System.out.println(job.toString());
                 //Instead of printing, this should insert the job listing information into the GUI
             }
 
@@ -86,14 +84,13 @@ public class MainActivity extends AppCompatActivity  implements AdapterView.OnIt
                 System.out.println("The read failed: " + databaseError.getCode());
             }
         });
-
-
     }
 
 
 
     private void readJobData() {
         // Read data from file
+        jobs = new ArrayList<Job>();
         InputStream is = getResources().openRawResource(R.raw.joblist);
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
         String line = "";
@@ -102,12 +99,26 @@ public class MainActivity extends AppCompatActivity  implements AdapterView.OnIt
                 // Split by '''
 
                 String[] fields = line.split("'''");
-                Job s = new Job(fields[0], fields[1], fields[2], fields[3], "idPlaceholder");
+                Job s = new Job(fields[0], fields[1], fields[2], fields[3], "idPlaceholder", Integer.parseInt(fields[4]));
                 jobs.add(s);
-
             }
         } catch(IOException e) {
             Log.e("MainActivity", "Error reading data on line " + line);
+        }
+
+        // Check if each job exists - if not, add to DB; if yes, print that it already exists
+        for(Job job: jobs)
+        {
+            System.out.println("Checking for job " + job.getlocalID());
+            if(!findJobByApiID(job.getlocalID()))
+            {
+                DatabaseReference pushedJobListing = database.child("JobListings").push();
+                job.setID(pushedJobListing.getKey());
+                pushedJobListing.setValue(job);
+            }
+            else
+                System.out.println("Job " + job.getlocalID() + " already exists in the database.");
+
         }
 
     }
@@ -141,6 +152,31 @@ public class MainActivity extends AppCompatActivity  implements AdapterView.OnIt
             }
         });
 
+    }
+
+    public boolean findJobByApiID(int ApiID)
+    {
+        jobExists = false;
+        Query resultList = database.child("JobListings").orderByChild("localID").equalTo(ApiID).getRef();
+        System.out.println("Query Resultset: " + resultList.toString());
+        resultList.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Job queryResult = dataSnapshot.getValue(Job.class);
+                System.out.println("Query result as a job object: " + queryResult);
+                if(queryResult != null) {
+                    jobExists = true;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        System.out.println("FindJob Method: jobExists evaluted to: " + jobExists); //ALWAYS FALSE - NEEDS FIX
+        return jobExists;
     }
 
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
